7c7a4e2290fecb022e209e26100722712236836a
Mohamed Mokdad <mokdad.mohamed05@gmail.com>
2022-04-19 22:51:49 +0100

    Create Min-MaxSUM.cs

---
+++ Min-MaxSUM.cs
@@ -0,0 +1,44 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<int> arr)
+    {
+        arr.Sort();
+        long sum= arr.Sum(x => (long)x);
+        long max = sum - arr.First();
+        long min = sum - arr.Last();
+        Console.WriteLine($"{min} {max}");
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.miniMaxSum(arr);
+    }
+}
