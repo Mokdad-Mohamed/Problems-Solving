5552ce95111b29a582f9b497e769f67eeeea298b
Mohamed Mokdad <mokdad.mohamed05@gmail.com>
2022-05-04 05:35:40 +0100

    Create GridChallenge.cs

---
+++ GridChallenge.cs
@@ -0,0 +1,76 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'gridChallenge' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING_ARRAY grid as parameter.
+     */
+
+    public static string gridChallenge(List<string> grid)
+    {
+        string result = "YES";
+        List<List<char>> sortedRows = new List<List<char>>();
+        foreach(string row in grid){
+            List<char> _row = row.ToCharArray().ToList();
+            _row.Sort();
+            sortedRows.Add(_row);
+        }
+        
+        for(int i=0; i<grid.Count()-1 ; i++){
+            for(int j=0; j<grid.Count() ; j++){
+                if(sortedRows[i][j]>sortedRows[i+1][j])
+                return "NO";
+            }
+            
+        }
+        
+        return result;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<string> grid = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string gridItem = Console.ReadLine();
+                grid.Add(gridItem);
+            }
+
+            string result = Result.gridChallenge(grid);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
